941f9e852bd3f2f2f9968058cc53432cd8e25947
Ravindra P <ravindra.p@niveussolutions.com>
2025-07-14 14:47:07 +0530

    added corwhitelist for fe repo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: allowedOrigins,
     credentials: true,
   });
 
